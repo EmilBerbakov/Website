6fa36342fb91e37d8a372d907f2b826d12d56958
EmilBerbakov <111311656+EmilBerbakov@users.noreply.github.com>
2022-08-24 00:08:08 -0500

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php $title="Emil's Library Database";$smallpicture="icons/bookicon.png";include("php-addons/top.php");?>
-<body>
-<h1>Home Base</h1>
-<?php include("php-addons/toolbar.php");include("php-addons/standardjsimport.php");?>
-</body>
-</html>
