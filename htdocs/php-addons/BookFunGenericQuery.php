a4ef8c12ba55e39ba2ed280a8840dfd23b77a351
EmilBerbakov <111311656+EmilBerbakov@users.noreply.github.com>
2022-08-30 12:07:57 -0500

    Add files via upload
    Added authorship information to initial search form

--- htdocs/php-addons/BookFunGenericQuery.php
+++ htdocs/php-addons/BookFunGenericQuery.php
@@ -4,9 +4,9 @@
 foreach($_POST as $key=>$value){
 		$column=$key;
 		$params=array($value);
+
 	};
 	
-
 
 
 //I am not letting DB searches on Title at this time.  This is due to the following:
@@ -14,7 +14,7 @@ foreach($_POST as $key=>$value){
 //2.) My personal machine has difficulty with handling external connections and external quieries that would take a while to resolve. The solution here would be to host my db on some cloud service, like Azure.
 //So, due to all of that, title searches will default to the RESTful lookup.  All other available search options are indexed, so I will allow db search first, then RESTful search if it's not on the db.
 
-if ($column!=='TITLE') {	
+//if ($column!=='TITLE') {	
 	include 'php-addons/BookFunGenericConnection.php';
 	try {
 		/*
@@ -39,26 +39,44 @@ if ($column!=='TITLE') {
 		
 		where $column=?
 		*/
-		/*
-		//For Author Output
-		$dbquery="select top 50 a.EDITION_ID,a.TITLE,a.SUBTITLE,a.ISBN_13,a.ISBN_10,c.AUTHOR_NAME from EDITION_INFO_2 a left join EDITION_TO_AUTHOR b on a.EDITION_ID=b.EDITION_ID left join AUTHOR_INFO c on b.AUTHOR_ID=c.AUTHOR_ID where a.$column=?";
-		*/
-		
-		//We will make sure $column is an expected column.  No Bobby Tables for my database...
 		if (in_array($column,array('TITLE','EDITION_ID','ISBN_10','ISBN_13'))==false){
 			$column='EDITION_ID';
 		}
+
+		if($key=='TITLE'){
+			$params[0]='"'.$params[0].'"';
+		};
+
+		//For Author Output
+		if($column !=='TITLE'){
+		$dbquery="select top 50 a.EDITION_ID,a.TITLE,a.SUBTITLE,a.ISBN_13,a.ISBN_10,c.AUTHOR_NAME from EDITION_INFO_2 a left join EDITION_TO_AUTHOR b on a.EDITION_ID=b.EDITION_ID left join AUTHOR_INFO c on b.AUTHOR_ID=c.AUTHOR_ID where a.$column=?";
+		}
+
+		else {
+			$dbquery="select a.EDITION_ID,a.TITLE,a.SUBTITLE,a.ISBN_13,a.ISBN_10,c.AUTHOR_NAME
+			from EDITION_INFO_2 a
+	
+			left join EDITION_TO_AUTHOR b on a.EDITION_ID=b.EDITION_ID
+			left join AUTHOR_INFO c on b.AUTHOR_ID=c.AUTHOR_ID
+			where contains(a.TITLE,?);";
+		}
+		
+		//We will make sure $column is an expected column.  No Bobby Tables for my database...
+
+		
+		
 		//For NON Author Output
 		
-		$dbquery="SELECT top 50 * FROM EDITION_INFO_2 where ".$column."=?";
+		//$dbquery="SELECT top 50 * FROM EDITION_INFO_2 where ".$column."=?";
+		
 		
 		$dbgetresults=$conn->prepare($dbquery);
 		$dbgetresults->execute($params);
 	
 		//For NON Author output
-	    $dbresults=$dbgetresults->fetchAll(PDO::FETCH_ASSOC);
+	    //$dbresults=$dbgetresults->fetchAll(PDO::FETCH_ASSOC);
 		//For Author output
-		//$dbresults=$dbgetresults->fetchAll(PDO::FETCH_GROUP | PDO::FETCH_ASSOC);
+		$dbresults=$dbgetresults->fetchAll(PDO::FETCH_GROUP | PDO::FETCH_ASSOC);
 		
 		$dbresultscount=count($dbresults);
 
@@ -72,12 +90,13 @@ if ($column!=='TITLE') {
 	catch(Exception $e){
 		print_r($e);
 	}
-}
+//}
+/*
 else{
 	$dbresultscount=0;
 	include 'BookFunGenericQueryRESTful.php';
 }
-
+*/
 
 ;?>
 
