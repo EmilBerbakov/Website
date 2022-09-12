9f8295fcf9ee9f17e6c9a7c2c6c48f64e8edafa0
EmilBerbakov <111311656+EmilBerbakov@users.noreply.github.com>
2022-09-11 22:14:23 -0500

    Update BookFunGenericConnection.php

--- php-addons/BookFunGenericConnection.php
+++ php-addons/BookFunGenericConnection.php
@@ -1,6 +1,6 @@
 <?php 
-$serverName="tcp:127.0.0.1g"; 
-$conn= new PDO("sqlsrv:server=$serverName;Database=Book_Fun","generic","sqlfun");
+$serverName="***"; 
+$conn= new PDO("***");
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 //Unless this is not set to true, db searches take a while;
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
