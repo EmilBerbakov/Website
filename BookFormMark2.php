dadc0b7dc716f4f159a159086c44c70b37796954
EmilBerbakov <111311656+EmilBerbakov@users.noreply.github.com>
2022-09-11 23:16:07 -0500

    Add files via upload
    aboutme - changed to horizontal card view with light border
    BookFormMark2 - changed intro text card borders to light.  Added vertical collapse animation

--- BookFormMark2.php
+++ BookFormMark2.php
@@ -12,19 +12,22 @@
 	<h1>Encyclopedia Berb's Library Database</h1>
 	<?php include("php-addons/toolbar.php");?> <br />
 	<button id='texttogglebutton' class='btn btn-dark' type='button' data-bs-toggle='collapse' data-bs-target='#introtext' aria-expanded='true' aria-controls='introtext' onclick='texttoggle()'> Hide Intro </button>
-	<div id='introtext' class='show collapse-horizontal'>
-	<div class='card card-body'>
-	
-	<p> I love to read.<br />
-	<p>I also love to buy books. Lots and lots of books. This has led to me forgetting what I already have when I am browsing the shelves of my favorite local bookstore. It was around the time I bought my third copy of The Silmarillion that I realized that my library is way too big to keep track of in my head. Now, I could write down all of my books in the notes app on my phone or use an online library, but then I wouldn't be able to flex my technical muscles to create this!
+	<div id='introtext' class='show collapse'>
+	<div class="card border-light" style="width: 70rem">
+	<div class='card-body'>
+	<p > I love to read.</p>
+	<p >I also love to buy books. Lots and lots of books. This has led to me forgetting what I already have when I am browsing the shelves of my favorite local bookstore. It was around the time I bought my third copy of The Silmarillion that I realized that my library is way too big to keep track of in my head. Now, I could write down all of my books in the notes app on my phone or use an online library, but then I wouldn't be able to flex my technical muscles to create this!
 	</p>
 	
-    Through this project, affectionately named "Encyclopedia Berb's Library Database" users can:</p>
+    <p >Through this project, affectionately named "Encyclopedia Berb's Library Database" users can:</p>
+	<p >
 	<ol>
 		<li>Search the SQL database for books, using both Open Library and RESTful to find and compile information within the database</li>
 		<li>Create an account to add books to a personal library, categorized by ownership and reading status </li>
 		<li> Keep track of personal libraries and reading progress through the database </li>
 	</ol>
+	</p>
+	</div>
 	</div>
 	</div>
 <br/>
